94f594e34baf3aa6f2d435dac96f71e2e57b88e0
Harini <harini9804@gmail.com>
2018-10-14 21:43:05 +0530

    label, colour changes

--- quick_sort_2.py
+++ quick_sort_2.py
@@ -2,6 +2,7 @@ from threading import Thread
 import threading
 import time
 import thread
+import numpy as np
 
 import Tkinter as tk
 
@@ -47,14 +48,14 @@ def qsort(arr,low,high):
 '''testing below'''
 ls = [10,5,1,3,6,4,15,9,2,13,8,12,16,7]
 n=len(ls)
+chk_arr = []
 root = tk.Tk()
 res = qsort(ls, 0, len(ls) - 1)
-
 for each in arr_itr:
     print " --> ",each[0],each[1],each[2], " "
-    labels.append( tk.Label(root, text=" ".join( str(each[0]) ) ) )
-    labels.append(tk.Label(root, text = "Pivot is: "+str(each[1])) )
-    labels.append(tk.Label(root, text =" ".join(str(each[2]))) )
+    labels.append( tk.Label(root, text=" ".join( str(each[0]) ),fg="blue" ) )
+    labels.append(tk.Label(root, text = "Pivot is: "+str(each[1]), fg = "red") )
+    labels.append(tk.Label(root, text =" ".join(str(each[2])),fg="blue" ) )
 
 i=0
 while(i<len(labels)-1):
@@ -63,6 +64,21 @@ while(i<len(labels)-1):
     labels[i+1].grid(row=i,column=2)
     labels[i+2].grid(row=i,column=4)
     i=i+3
+
+
+        # big_arr = arr_itr[i]
+        # chk_arr.extend(big_arr[0])
+        # chk_arr.append(big_arr[1])
+        # chk_arr.extend(big_arr[2])
+        # prev_arr = arr_itr[i-1]
+        # if np.array_equal(chk_arr,prev_arr[0]):
+        #     labels[i].grid(row=i,column = 0)
+        #     col = col/2
+        # else:
+        #     labels[i].grid(row=i,column = col/2+1)
+
+label = tk.Label(root,text = "Sorted array: "+ " ".join(str(res)),fg = "green"  )
+label.grid(row=len(labels),column = 2)
 
 root.mainloop()
 print(res)
