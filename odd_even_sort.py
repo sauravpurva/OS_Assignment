7b30624759c05598452b528313928a103b348657
Harini <harini9804@gmail.com>
2018-10-14 15:56:32 +0530

    Add files via upload

--- odd_even_sort.py
+++ odd_even_sort.py
@@ -1,9 +1,8 @@
 import threading as th
 import merge_thread as mt
+import Queue
 
-
-
-def oddEvenSort(arr):
+def oddEvenSort(arr,out_queue):
     # Initially array is unsorted
     isSorted = 0
     n = len(arr)
@@ -21,34 +20,38 @@ def oddEvenSort(arr):
                 isSorted = 0
 
     print "hello",arr,"hello\n"
-    return arr
-
+    out_queue.put(arr)
 
 
 arr = [15,12,70,32,115,24,8,64,16,128,51,52,84,23,96,6,97,3,99,0]
 thr = []
 afterOE = []
-x=0
-thr.append(th.Thread(target=oddEvenSort,args=(arr[0:4],)) )
-thr.append(th.Thread(target=oddEvenSort,args=(arr[4:8],)) )
-thr.append(th.Thread(target=oddEvenSort,args=(arr[8:12],)) )
-thr.append(th.Thread(target=oddEvenSort,args=(arr[12:16],)) )
-thr.append(th.Thread(target=oddEvenSort,args=(arr[16:20],)) )
+x = len(arr) % 4
+my_queue = Queue.Queue()
+
+i=0
+while( i< len(arr) ):
+    print "i is ",i
+    thr.append(th.Thread(target=oddEvenSort, args=(arr[i:i+4], my_queue, )))
+    i = i+4
+
+# thr.append(th.Thread(target=oddEvenSort,args=(arr[0:4],my_queue,)) )
+# thr.append(th.Thread(target=oddEvenSort,args=(arr[4:8],my_queue,)) )
+# thr.append(th.Thread(target=oddEvenSort,args=(arr[8:12],my_queue,)) )
+# thr.append(th.Thread(target=oddEvenSort,args=(arr[12:16],my_queue,)) )
+# thr.append(th.Thread(target=oddEvenSort,args=(arr[16:20],my_queue,)) )
 
 for thread in thr:
     thread.start()
     x=x+1
 
-print "no of threads", x
-
-
 for thread in thr:
     ret = thread.join()
-    print ret,"\n"
 
-print afterOE, " is the combined array\n"
+while(my_queue.empty() == False ):
+    afterOE.extend(my_queue.get())
+print "afterOE is ", afterOE
 
-print "after threads",arr
-mt.mergeSort(arr)
+mt.mergeSort(afterOE)
 
-print "after mergesort", arr
+print "after mergesort", afterOE
