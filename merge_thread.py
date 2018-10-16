69cb4ce60f63f834274ad83d28fca039f94e9282
Harini <harini9804@gmail.com>
2018-10-16 18:24:28 +0530

    pygame

--- merge_thread.py
+++ merge_thread.py
@@ -1,11 +1,30 @@
+from threading import Thread
 import threading as th
+import time
+import thread
+import numpy as np
+
+import Tkinter as tk
+import pygame
+from pygame.locals import *
+
+scr_size = (width,height) = (900,600)
+FPS = 0.5
+screen = pygame.display.set_mode(scr_size)
+clock = pygame.time.Clock()
+black = (0,0,0)
+white = (255,255,255)
+green = (0,255,0)
+blue = (0,0,255)
+
+pygame.display.set_caption('Merge Sort')
 
 def mergeSort(arr):
     if len(arr)>1:
         mid = len(arr)/2
         lefthalf = arr[:mid]
         righthalf = arr[mid:]
-
+        displayarray(arr,mid-1)
         t1 = th.Thread(target=mergeSort, args=(lefthalf,))
         t2 = th.Thread(target=mergeSort, args=(righthalf,))
 
@@ -40,9 +59,50 @@ def mergeSort(arr):
             arr[k]=righthalf[j]
             j=j+1
             k=k+1
+        displayarray(arr,mid)
 
-        # print "merging", arr
+def displayarray(arr,mid):
+    image = pygame.Surface((width - width/5,height - height/5))
+    rect = image.get_rect()
+    rect.top = height/10
+    rect.left =  width/10
+    width_per_bar = rect.width/len(arr) - 2
 
-#
-# arr = [13,4,1,7,90,24,113,63]
-# mergeSort(arr)
+    l = 0
+    mid = len(arr)/2 -1
+    for k in range(0,rect.width,width_per_bar + 2):
+        bar = pygame.Surface((width_per_bar,arr[l]*10))
+        bar_rect = bar.get_rect()
+        if(l <= mid):
+            bar.fill(blue)
+        else:
+            bar.fill(green)
+        bar_rect.bottom = rect.height
+        bar_rect.left = k
+
+        image.blit(bar,bar_rect)
+        l += 1
+        if l == len(arr):
+            break
+
+
+    screen.fill(black)
+    screen.blit(image,rect)
+    pygame.display.update()
+    clock.tick(FPS)
+
+'''testing below'''
+arr = [100,5,1,3,6,16,4,15,9,30,2,13,8,12,16,7]
+n=len(arr)
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+        if event.type == pygame.KEYDOWN:
+            pass
+        if event.type == pygame.KEYUP:
+            pass
+        if sorted(arr) != arr:
+            mergeSort(arr)
+        else:
+            displayarray(arr,n/2 - 1)
